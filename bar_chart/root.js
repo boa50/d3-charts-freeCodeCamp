4391f3ccbec24193d43ff238348fe45cce64c9cc
boa50 <bruno.albuquerque50@gmail.com>
2023-08-10 10:48:14 +0930

    Gave a labet to the y axis

--- bar_chart/root.js
+++ bar_chart/root.js
@@ -8,7 +8,13 @@ async function getData() {
 
 const WIDTH = 600
 const HEIGHT = 350
-const PADDING = 48
+
+const PADDING = {
+    left: 100,
+    right: 16,
+    top: 16,
+    bottom: 16
+}
 
 const title = d3.select("body")
     .append("h1")
@@ -21,14 +27,21 @@ const svg = d3.select("body")
     .attr("width", WIDTH)
     .attr("height", HEIGHT)
 
+svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", PADDING.left / 4)
+    .attr("x", 0 - (HEIGHT / 2))
+    .style("text-anchor", "middle")
+    .text("GDP")
+
 getData().then(dataset => {
     const xScale = d3.scaleTime()
         .domain([d3.min(dataset, d => d[0]), d3.max(dataset, d => d[0])])
-        .range([PADDING, WIDTH - PADDING])
+        .range([PADDING.left, WIDTH - PADDING.right])
 
     const yScale = d3.scaleLinear()
         .domain([0, d3.max(dataset, d => d[1])])
-        .range([HEIGHT - PADDING, PADDING])
+        .range([HEIGHT - PADDING.top, PADDING.bottom])
 
     const xAxis = d3.axisBottom(xScale)
     const yAxis = d3.axisLeft(yScale).tickFormat(d3.format("$,.2f"))
@@ -41,7 +54,7 @@ getData().then(dataset => {
         .attr("x", d => xScale(d[0]))
         .attr("y", d => yScale(d[1]))
         .attr("width", 2)
-        .attr("height", d => HEIGHT - yScale(d[1]) - PADDING)
+        .attr("height", d => HEIGHT - yScale(d[1]) - PADDING.top)
         .attr("data-date", d => d[0].toISOString().substring(0, 10))
         .attr("data-gdp", d => d[1])
         .on("mouseover", mouseover)
@@ -50,11 +63,11 @@ getData().then(dataset => {
 
     svg.append("g")
         .attr("id", "x-axis")
-        .attr("transform", "translate(0, " + (HEIGHT - PADDING) + ")")
+        .attr("transform", "translate(0, " + (HEIGHT - PADDING.top) + ")")
         .call(xAxis)
 
     svg.append("g")
         .attr("id", "y-axis")
-        .attr("transform", "translate(" + PADDING + ", 0)")
+        .attr("transform", "translate(" + PADDING.left + ", 0)")
         .call(yAxis)
 })
