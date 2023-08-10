7c179a54f8cf8dbd52f7ea698bc233dd3b0cecf9
boa50 <bruno.albuquerque50@gmail.com>
2023-08-10 10:24:45 +0930

    Formatted the currencies

---
+++ bar_chart/root.js
@@ -0,0 +1,60 @@
+async function getData() {
+    let res = await fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json")
+    let resJson = await res.json()
+    let dataset = resJson.data
+        .map(x => [new Date(x[0] + "T00:00:00Z"), x[1]])
+    return dataset
+}
+
+const WIDTH = 600
+const HEIGHT = 350
+const PADDING = 48
+
+const title = d3.select("body")
+    .append("h1")
+    .attr("id", "title")
+    .text("USA GDP")
+
+const svg = d3.select("body")
+    .append("svg")
+    .attr("id", "chart")
+    .attr("width", WIDTH)
+    .attr("height", HEIGHT)
+
+getData().then(dataset => {
+    const xScale = d3.scaleTime()
+        .domain([d3.min(dataset, d => d[0]), d3.max(dataset, d => d[0])])
+        .range([PADDING, WIDTH - PADDING])
+
+    const yScale = d3.scaleLinear()
+        .domain([0, d3.max(dataset, d => d[1])])
+        .range([HEIGHT - PADDING, PADDING])
+
+    const xAxis = d3.axisBottom(xScale)
+    const yAxis = d3.axisLeft(yScale).tickFormat(d3.format("$,.2f"))
+
+    svg.selectAll("rect")
+        .data(dataset)
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", d => xScale(d[0]))
+        .attr("y", d => yScale(d[1]))
+        .attr("width", 2)
+        .attr("height", d => HEIGHT - yScale(d[1]) - PADDING)
+        .attr("data-date", d => d[0].toISOString().substring(0, 10))
+        .attr("data-gdp", d => d[1])
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave)
+
+    svg.append("g")
+        .attr("id", "x-axis")
+        .attr("transform", "translate(0, " + (HEIGHT - PADDING) + ")")
+        .call(xAxis)
+
+    svg.append("g")
+        .attr("id", "y-axis")
+        .attr("transform", "translate(" + PADDING + ", 0)")
+        .call(yAxis)
+})
---
+++ bar_chart/tooltip.js
@@ -0,0 +1,35 @@
+const getQuarter = dt => `Q${Math.floor(dt.getMonth() / 3 + 1)} ${dt.getFullYear()}`
+
+const currencyFormatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 1,
+    maximumFractionDigits: 1
+})
+
+const tooltip = d3.select("body")
+    .append("div")
+    .attr("id", "tooltip")
+
+const mouseover = function (event, d) {
+    tooltip
+        .style("opacity", 1)
+        .attr("data-date", d[0].toISOString().substring(0, 10))
+    d3.select(this)
+        .style("stroke", "black")
+        .style("opacity", 1)
+}
+
+const mousemove = function (event, d) {
+    tooltip
+        .html(`${getQuarter(d[0])} <br> GDP: ${currencyFormatter.format(d[1])}B`)
+        .style("left", (d3.pointer(event)[0] + 20) + "px")
+        .style("top", (d3.pointer(event)[1] + 50) + "px")
+}
+
+const mouseleave = function (event, d) {
+    tooltip
+        .style("opacity", 0)
+    d3.select(this)
+        .style("stroke", "none")
+}
